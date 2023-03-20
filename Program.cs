b5f86041c17b77c3166cc17e4e51ab766ff07442
Even <78569926+even01@users.noreply.github.com>
2023-03-19 23:34:41 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("---- MINHAS FRUTAS FAVORITAS ----");
+Console.WriteLine("1 - Uva");
+Thread.Sleep(1000);
+Console.WriteLine("2 - Manga");
+Thread.Sleep(1000);
+Console.WriteLine("3 - Abacaxi");
+Thread.Sleep(1000);
+Console.WriteLine("4 - Abacate");
+Thread.Sleep(1000);
+Console.WriteLine("5 - Laranja");
